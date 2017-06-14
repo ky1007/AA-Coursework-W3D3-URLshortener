66730048c793a2b7319b218eaaf8d47b7746781d
Kevin Yang <kevinXyang@outlook.com>
2017-06-14 15:46:50 -0700

    Migrate visit join table

--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614211350) do
+ActiveRecord::Schema.define(version: 20170614223942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20170614211350) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer  "shortened_url_id", null: false
+    t.integer  "user_id",          null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["shortened_url_id"], name: "index_visits_on_shortened_url_id", using: :btree
+    t.index ["user_id"], name: "index_visits_on_user_id", using: :btree
   end
 
 end
