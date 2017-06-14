37898f4551b2eeb7d26da039832958d8655aa62c
Kevin Yang <kevinXyang@outlook.com>
2017-06-14 16:07:35 -0700

    Create Visit class associations

--- URLShortener/app/models/visit.rb
+++ URLShortener/app/models/visit.rb
@@ -1,3 +1,20 @@
 class Visit < ApplicationRecord
-  
+
+  validates
+
+  def self.record_visits!(user, shortened_url)
+    Visit.create!(user_id: user.id, shortened_url_id: shortened_url.id)
+  end
+
+  belongs_to :user,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+
+  belongs_to :shortened_url,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :ShortenedUrl
+
 end
